a8763476d432d2fd6a72d08ff82587fc77660fb9
Andrey <apiddubnij21@gmail.com>
2024-01-20 19:39:13 -0500

    Initial Commit

---
+++ Migliore/MiglioreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiglioreApp.swift
+//  Migliore
+//
+//  Created by Andrii Piddubnyi on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MiglioreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
